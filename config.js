4633cadc6933e28c6e5559fdd4dd603ef10f6e80
shasni-md <shasnimd1@gmail.com>
2025-05-03 08:25:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,8 @@ STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
 FOOTER: process.env.FOOTER || "Didula MD V2 💚",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
+AUTO_VOICE: process.env.AUTO_VOICE || "true", // true or false
+AUTO_REPLY: process.env.AUTO_REPLY || "true", // true or false      
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
 SESSION_ID: process.env.SESSION_ID || "Y6kWhCSZ#I5K94YzeiMn1i9foCWA-kRKqQ_qupEUAOTa_XNuEyb0",
 OWNER_NUMBER: process.env.SESSION_ID || "94783919841",
