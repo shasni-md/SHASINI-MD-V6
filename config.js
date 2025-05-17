f18ace27b52793c03fd73464148ae2adb029bd4e
shasni-md <shasnimd1@gmail.com>
2025-05-17 08:38:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ AUTO_VOICE: process.env.AUTO_VOICE || "true", // true or false
 AUTO_REPLY: process.env.AUTO_REPLY || "true", // true or false   
 AUTO_STICKER: process.env.AUTO_STICKER || "true", // truu or false    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "FUQAxTKT#EuXII4BQSDU2i-xBPd8HvMGFIJey6p8yAcCushj3Tuc",
+SESSION_ID: process.env.SESSION_ID || "8dQHzJwQ#QnhBvlWv9-A1ek5rflgQvQX2dXJKwPbculhrPn54aDU",
 OWNER_NUMBER: process.env.SESSION_ID || "94783919841",
 PREFIX: process.env.PREFIX || ".",
 ANTI_DELETE: process.env.ANTI_DELETE || "true"
